ff3d752e18823376823ed83103fb77f6802e6c71
Boemeltrein <130394941+Boemeltrein@users.noreply.github.com>
2026-01-25 13:42:30 +0100

    Update download_talosctl.sh

--- clustertool/embed/download_talosctl.sh
+++ clustertool/embed/download_talosctl.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 # Define the version
 ## ADDED By BOEMELTREIN
-# renovate: datasource=github-releases depName=ghcr.io/siderolabs/installer versioning=v<semver>
+# renovate: datasource=docker depName=ghcr.io/siderolabs/installer versioning=v<semver>
 version="1.12.1"
 
 # Define the OS and architecture combinations
